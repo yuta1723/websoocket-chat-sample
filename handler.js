08e2eb378bcadca6a522847c7f1e2d03144d1091
yuta1723 <yuta.n1723@gmail.com>
2019-07-10 15:57:09 +0900

    Revert "仕様変更に伴い、handler名をjoinからstartRequestに変更した"
    This reverts commit 3b4f0a79b12a4bcc733666f732c519d0d3de7066.

--- handler.js
+++ handler.js
@@ -7,7 +7,6 @@ var DDB = new AWS.DynamoDB({ apiVersion : "2012-10-08"});
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 const ROOM_TABLE_NAME = 'websocket-room-table';
-const CONNECTION_ID_TABLE_NAME = 'websocket-connection-table';
 
 
 exports.connect = async (event) => {
@@ -21,15 +20,15 @@ exports.connect = async (event) => {
     return response;
 };
 
-exports.startRequest = async (event, context) => {
+exports.joinRoom = async (event) => {
 
-    let roomId = event.roomId;
-    let accountId = event.accountId;
+    let roomId = event.queryStringParameters.roomId;
+    let businessId = event.queryStringParameters.businessId;
     let subRoomId = '0';
 
-    console.log('roomId = ' + roomId + 'accountId = ' + accountId);
+    console.log('roomId = ' + roomId + 'businessId = ' + businessId);
 
-    let uniqueRoomId = accountId + '_' + roomId + '_' + subRoomId;
+    let uniqueRoomId = businessId + '_' + roomId + '_' + subRoomId;
 
     let getParams = {
         TableName : ROOM_TABLE_NAME,
@@ -43,36 +42,20 @@ exports.startRequest = async (event, context) => {
         Item: {
             uniqueRoomId : uniqueRoomId,
             roomId : roomId,
-            businessId : accountId,
+            businessId : businessId,
             subRoomId: subRoomId
         }
     };
 
     var roomData = await docClient.get(getParams).promise();
 
-    // Room テーブルにルームがすでに追加されているかを確認。
     console.log('roomData = ' + JSON.stringify(roomData, null, 2));
     if (isEmptyJson(roomData)) {
         console.log('EMPTY');
-        // 追加されてなかったら追加
         var data = await docClient.put(putParams).promise();
         console.log('data = ' + JSON.stringify(data, null, 2));
     }
 
-    let connectionId = event.requestContext.connectionId;
-    console.log('connectionId = ' + connectionId);
-
-    // Connection テーブルにConnectionを追加。
-    let connectionParam = {
-        TableName : CONNECTION_ID_TABLE_NAME,
-        Item:{
-          connectionId : connectionId,
-          uniqueRoomId : uniqueRoomId
-        }
-    };
-
-    var connectionData = await docClient.put(connectionParam);
-    console.log('connectionData = ' + JSON.stringify(connectionData, null, 2));
 };
 
 function isEmptyJson(obj){
