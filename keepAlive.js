d9a45632b92c29f896eda0783d02d486173c156c
yuta1723 <yuta.n1723@gmail.com>
2019-07-18 15:39:30 +0900

    keepAlive用のlambdaを実装

---
+++ keepAlive.js
@@ -0,0 +1,20 @@
+var AWS = require ('aws-sdk');
+AWS.config.update({ region : process.env.AWS_REGION});
+
+const TAG = '[KEEP_ALIVE]';
+
+const VERSION = 2;
+
+exports.default = async (event) => {
+
+    // キープアライブは、デバッグ版のみAPI GW → Lambdaとのやり取りを許容する。
+    // 本番環境には基本的には実装しない。
+
+    console.log(TAG + ' event =' + JSON.stringify(event));
+
+    var response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
