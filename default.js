0439526d52dee6556562d96d06feec3941af2829
yuta1723 <yuta.n1723@gmail.com>
2019-07-18 15:36:47 +0900

    default.jsにエラー処理を追加した

--- default.js
+++ default.js
@@ -8,6 +8,29 @@ const VERSION = 2;
 exports.default = async (event) => {
     console.log(TAG + ' event =' + JSON.stringify(event));
 
+
+    var connectionId = event.requestContext.connectionId;
+
+    var apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: "2018-11-29",
+        endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
+    });
+
+    // event の body の内容次第で返却するメッセージを変える。
+    // commandTypeが取得できたら、エラーメッセージを変更する。
+    var pushData = {
+        commandType: 'error',
+        statusCode: '399999',
+        message:'defaultラムダが発火されました。'
+    };
+    var params = {
+        Data: JSON.stringify(pushData),
+        ConnectionId : connectionId
+
+    };
+    console.log('SEND MESSAGE params= ' + JSON.stringify(params));
+    await apigwManagementApi.postToConnection(params).promise();
+
     // レスポンス返さないとエラーになる。
     var response = {
         statusCode: 200,
