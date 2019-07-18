6a1fb9faae39c5e84e474989ad692a7543320ed8
yuta1723 <yuta.n1723@gmail.com>
2019-07-18 15:28:32 +0900

    defaultとsendMessageを区別した

--- default.js
+++ default.js
@@ -1,58 +1,12 @@
 var AWS = require ('aws-sdk');
 AWS.config.update({ region : process.env.AWS_REGION});
 
-var docClient = new AWS.DynamoDB.DocumentClient();
-
 const TAG = '[DEFAULT]';
 
 const VERSION = 2;
 
 exports.default = async (event) => {
     console.log(TAG + ' event =' + JSON.stringify(event));
-
-    var apigwManagementApi = new AWS.ApiGatewayManagementApi({
-        apiVersion: "2018-11-29",
-        endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
-    });
-
-    var connectionId = event.requestContext.connectionId;
-    var uniqueRoomId = JSON.parse(event.body).uniqueRoomId;
-    console.log(TAG + ' connectionID = ' + connectionId + ' uniqueRoomId = ' + uniqueRoomId);
-
-
-    // ルームにいるユーザーを取得
-    var scanParam = {
-        TableName : process.env.CONNECTION_TABLE,
-        FilterExpression: 'uniqueRoomId = :roomId',
-        ExpressionAttributeValues:{
-            ':roomId': uniqueRoomId
-        }
-    };
-    let connections = await docClient.scan(scanParam).promise();
-    console.log(TAG + 'connections = '+ JSON.stringify(connections.Items));
-
-    var pushData = {
-        commandType: 'deliverMessage',
-        message: JSON.parse(event.body).message
-    };
-    await Promise.all(connections.Items.map(async ({ connectionId }) => {
-        try {
-            var params = {
-                Data: JSON.stringify(pushData),
-                ConnectionId : connectionId
-
-            };
-            console.log('SEND MESSAGE params= ' + JSON.stringify(params));
-            await apigwManagementApi.postToConnection(params).promise();
-        } catch (e) {
-            console.log('ERROR');
-            if (e.statusCode === 410) {
-                // await dynamodb.delete({ TableName: process.env.CONNECTIONS_TABLE, Key: { ConnectionId: ConnectionId }}).promise();
-            } else {
-                throw e;
-            }
-        }
-    }));
 
     // レスポンス返さないとエラーになる。
     var response = {
